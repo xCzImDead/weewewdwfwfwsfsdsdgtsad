d649b3b144562752b10a449f3cbf9a006c7d1899
sccishere <40580750+sccishere@users.noreply.github.com>
2018-07-01 16:32:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -366,11 +366,11 @@ client.on('message', async message => {
     let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!hugUser) return message.channel.send("Make sure you mention someone!");
     const {body} = await superagent
-    .get(`https://nekos.life/api/v2/img/hug`);
+    .get(`https://cdn.nekos.life/hug/hug11596.gif`);
     let hugEmbed = new Discord.RichEmbed()
     .setTitle("Hug! c:")
     .setDescription(`**${message.author.username}** hugged **${message.mentions.users.first().username}**!`)
-    .setImage('https://cdn.discordapp.com/attachments/460166361625264140/462769611235000330/image.png')
+    .setImage('')
     .setColor("RANDOM")
     .setFooter("Panda", client.user.displayAvatarURL);
 
