da9bd4bdd1515cbf5466c9a38f50a712cf564d5f
sccishere <40580750+sccishere@users.noreply.github.com>
2018-07-01 16:36:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -370,7 +370,7 @@ client.on('message', async message => {
     let hugEmbed = new Discord.RichEmbed()
     .setTitle("Hug! c:")
     .setDescription(`**${message.author.username}** hugged **${message.mentions.users.first().username}**!`)
-    .setImage('')
+    .setImage('https://cdn.discordapp.com/attachments/460166361625264140/462769611235000330/image.png,https://cdn.discordapp.com/attachments/460166361625264140/462769580750929921/image.jpg')
     .setColor("RANDOM")
     .setFooter("Panda", client.user.displayAvatarURL);
 
